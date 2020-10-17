88e1fa6b10e7c7746e06c55b646e278269936be7
LynnKomang <lynnkomang98@gmail.com>
2020-10-17 17:04:08 +0300

    Added basic info display

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>New Confirmed: {{ info.NewConfirmed }}</div>
+  <div
+    class="animate__animated animate__fadeInDown md:flex border-2 border-gray-400 shadow-xl rounded-lg divide-y md:divide-x md:divide-y-0 divide-gray-400"
+  >
+    <div class="text-center mx-auto pb-4 w-full">
+      <div class="text-6xl">{{ info.NewDeaths }}</div>
+      <div class="text-3xl">New deaths 💀️</div>
+    </div>
+
+    <div class="text-center mx-auto pb-4 w-full">
+      <div class="text-6xl">{{ info.NewConfirmed }}</div>
+      <div class="text-3xl">New cases 😷️</div>
+    </div>
+
+    <div class="text-center mx-auto pb-4 w-full">
+      <div class="text-6xl">{{ info.NewRecovered }}</div>
+      <div class="text-3xl">New recovered 😁️</div>
+    </div>
+  </div>
 </template>
 
 <script>
