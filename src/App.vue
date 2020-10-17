f596ea44e9a12d850ccff4c89b24176c9a1cfe4f
LynnKomang <lynnkomang98@gmail.com>
2020-10-17 17:57:27 +0300

    Implemented header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app" class="container mx-auto py-20 px-4 md:px-0">
-    <Info />
+  <div id="app">
+    <Header />
+    <div class="container mx-auto py-20 px-4 md:px-0">
+      <Info />
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
 import Info from "./components/Info";
 
 export default {
   name: "App",
   components: {
+    Header,
     Info,
   },
 };
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-gradient-to-r from-red-600 to-red-400 pl-10 md:pl-24 text-4xl md:text-6xl items-center shadow-xl py-6 md:py-10"
+  >
+    COVID-19 Update
+  </div>
+</template>
