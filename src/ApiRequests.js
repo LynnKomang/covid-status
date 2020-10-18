2a102cb6fdd9e6c48092e86ec7c1e15a3529e6ab
LynnKomang <lynnkomang98@gmail.com>
2020-10-18 18:22:25 +0300

    Moved articles request to the backend server

--- src/ApiRequests.js
+++ src/ApiRequests.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import config from '../config';
 
 export const getDetails = async (countryName) => {
 	return (await axios.get('https://api.covid19api.com/summary')).data.Countries.find(
@@ -8,9 +7,5 @@ export const getDetails = async (countryName) => {
 };
 
 export const getArticles = async () => {
-	return (
-		await axios.get(
-			`https://newsapi.org/v2/everything?apiKey=${config.API_KEY}&q=קורונה&sortBy=publishedAt`
-		)
-	).data.articles.slice(0, 10);
+	return (await axios.get(`${process.env.VUE_APP_ADDRESS}/articles`)).data;
 };
